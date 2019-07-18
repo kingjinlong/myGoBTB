c8bee557ccac4d7e0d654b333bf390610911e4cd
jl <411195527@qq.com>
2019-07-18 22:15:57 +0800

    add new

--- test_tick.py
+++ test_tick.py
@@ -58,84 +58,60 @@ def test_movement_thread():
         # for j in range(1,cc_data.columns.size-1,1):
 
 def test_movement_thread2():
-    print("测试tick策略")
+    print("测试对冲策略")
     date_File = "cu_min.csv"
     cc_data = pd.DataFrame(pd.read_csv(date_File))
-    # cc_data = origin_data.set_index('datetime')
-    print(cc_data)
-    
     N=len(cc_data)
     R = 10
+    H = 5
 
     print(N)
     print(cc_data.columns)
 
     aa_data = pd.DataFrame()
-    aa_data["datetime"] = cc_data['datetime']
-    aa_data["m"] = cc_data['DCE.m1905.close']
-    aa_data["RM"] = cc_data['CZCE.RM905.close']
-    aa_data["y"] = cc_data['DCE.y1905.close']
-    aa_data["p"] = cc_data['DCE.p1905.close']
-    aa_data["OI"] = cc_data['CZCE.OI905.close']
+    aa_data["datetime"] = cc_data['datetime'][0:500]
+    aa_data["m"] = cc_data['DCE.m1905.close'][0:500]
+    aa_data["RM"] = cc_data['CZCE.RM905.close'][0:500]
+    aa_data["y"] = cc_data['DCE.y1905.close'][0:500]
+    aa_data["p"] = cc_data['DCE.p1905.close'][0:500]
+    aa_data["OI"] = cc_data['CZCE.OI905.close'][0:500]
     aa_data = aa_data.set_index('datetime')
     #提取商品名字
     commodity_name = aa_data.columns.values.tolist()  
     spread_data = aa_data.diff(periods = R)
     print(spread_data)
 
-    ###创建一个空的order数据框
-    positable=np.zeros(shape=(len(aa_data),aa_data.columns.size))
+    # positable=np.zeros(shape=(len(aa_data),aa_data.columns.size))
+    positable=np.zeros(shape=(0,aa_data.columns.size))
     df_empty = pd.DataFrame(positable)
     df_empty.columns=commodity_name
     # df_empty['datetime']=cc_data['datetime']
     # df_empty = df_empty.set_index('datetime')
-    print(df_empty)
 
-
+    df_pos = pd.DataFrame(positable)
+    df_pos.columns = commodity_name
 
     ii = 0
     for index, row in spread_data.iterrows():
         aa = [i[0] for i in sorted(enumerate(row), key=lambda x:x[1])]
-        print(str(index) + "    "+ str(aa[0]))
-        for i in range(len(aa)):
-            df_empty.iloc[ii,i] = aa[i]
-        if ii>20:
-            break
-        ii = ii+1
-
-    # print(df_empty )
-
-# #创建一个空的Dataframe
-# result =pd.DataFrame(columns=('idx','degree','weight','diameter'))
-# #将计算结果逐行插入result,注意变量要用[]括起来,同时ignore_index=True，否则会报错，ValueError: If using all scalar values, you must pass an index
-# for i in idx:
-#     degree=
-#     weight=
-#     diameter=
-#     result=result.append(pd.DataFrame({'idx':[i],'degree':[degree],'weight':[weight],'diameter':[diameter]}),ignore_index=True
-
-# df.loc[i] = kjcgml.loc[i].append(bzwzcgml.loc[j])
-
-
-
-    # #删除中文字，日期
-    # del commodity_name[0]        
-    # #计算每个品种每隔R天的收益率
-    # rate_return=[]
-    # for index, value in enumerate(commodity_name):
-    #     rate_list=[]    
-    #     for i in range(R-1,N,1):    
-    #         print(aa_data[value][i])        
-    #         rate=(aa_data[value][i]-aa_data[value][i-(R-1)])/aa_data[value][i-(R-1)]
-    #         rate_list.append(rate)
-    #     rate_return.append(rate_list)
-
-
-a = [3,4,2,5,6]
-
+        df_empty.loc[index] = aa
+        bb = []
+        if ii <R:
+            bb = [0,0,0,0,0]
+        else:
+            if (ii - R) % H == 0:
+                #换仓点
+                for i in range(len(aa)):
+                    if aa[i] == 0: bb.append(1)
+                    if aa[i] == 1: bb.append(1)
+                    if aa[i] == 2: bb.append(0)
+                    if aa[i] == 3: bb.append(-1)
+                    if aa[i] == 4: bb.append(-1)
+            else:
+                bb = df_pos.iloc[ii - 1].tolist()
+        df_pos.loc[index] = bb
+        ii = ii +1
+    print(df_pos)
+  
 test_movement_thread2()
-
-myList = [1, 2, 3, 100, 5]
-aa = [i[0] for i in sorted(enumerate(myList), key=lambda x:x[1])]
-# [0, 1, 2, 4, 3]
-print(aa)
+print('ALL IS END......')
